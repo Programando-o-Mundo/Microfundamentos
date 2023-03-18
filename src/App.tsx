849cba09b42143b850dba393f1cfc3d44a94df2a
GustavoLR548 <gustavolr035@gmail.com>
2023-03-18 12:16:54 -0300

    teste do github actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1 className="text-lg">Projeto Microfundamentos</h1>
       <p>Seja bem vindo ao projeto Microfundamentos</p>
+      <p>isso aqui e um teste do github actions</p>
     </div>
   );
 }
